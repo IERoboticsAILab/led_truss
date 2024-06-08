f79ece78b42ea6c87c5ffe32b4ad75df0312a3c8
Edu <edcafenet@gmail.com>
2024-06-08 21:45:38 +0200

    added new functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import math
 from rpi_ws281x import *
 
 LED_COUNT      = 60         # Number of LED pixels.
@@ -11,7 +12,6 @@ LED_CHANNEL    = 0          # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
-
 
 def activate_all():
     for i in range(LED_COUNT):
@@ -30,6 +30,84 @@ def clear_all():
         strip.setPixelColor(i, Color(0,0,0))
     strip.show()
 
+def glow(color, delay=0.05):
+    #Fade In.
+    for i in range (0, 256):
+        r = int(math.floor((i / 256.0) * color.r))
+        g = int(math.floor((i / 256.0) * color.g))
+        b = int(math.floor((i / 256.0) * color.b))
+        set_colour_all(Color(r, g, b))
+        strip.show()
+        time.sleep(delay)
+    #Fade Out.
+    for i in range (256, 0, -1):
+        r = int(math.floor((i / 256.0) * color.r))
+        g = int(math.floor((i / 256.0) * color.g))
+        b = int(math.floor((i / 256.0) * color.b))
+        set_colour_all(Color(r, g, b))
+        strip.show()
+        time.sleep(delay)
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+def theaterChase(color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, color)
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, 0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
+def rainbow(wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+
+def rainbowCycle(wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel(
+                (int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+
+def theaterChaseRainbow(wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, wheel((i + j) % 255))
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i + q, 0)
 
 # pass in values from 0-1. overflows correctly (eg can go from .8->.2 across the edge)
 def set_colour_range_percent(colour, start_percent, end_percent):
@@ -72,3 +150,4 @@ def angry_mode(delay = 0.05, duration = 30, width = 1):
         index = (index + 1) % LED_COUNT
         duration -= delay
         time.sleep(delay)
+
