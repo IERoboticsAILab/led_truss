629c5493d1f638765aee87b723acd9b16fe19e94
IE Robotics & AI Lab <ieuroboticslab@gmail.com>
2025-02-21 15:37:26 +0000

    Adding Suzan's code

---
+++ iLed/off.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import time
+from rpi_ws281x import *
+import argparse
+import signal
+import sys
+
+# LED strip configuration:
+LED_COUNT = 900  # Number of LED pixels.
+LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10  # DMA channel to use for generating a signal (try 10)
+LED_BRIGHTNESS = 160  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+def turnOffLights(strip):
+    """Turn off all the lights."""
+    colorWipe(strip, Color(0, 0, 0), 10)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+    
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+    
+    if args.clear:
+        signal.signal(signal.SIGINT, signal_handler)
+    
+    turnOffLights(strip)
