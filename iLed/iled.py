32ee277f4456cc646bbb1ccfe941d1102ef888bb
farah orfaly <farah.orfaly@gmail.com>
2025-03-05 16:40:01 +0100

    removing .clear()

--- iLed/iled.py
+++ iLed/iled.py
@@ -113,6 +113,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Clearing LEDs")
         colorWipe(strip, Color(0,0,0), 10)
-        strip.clear()
         strip.show()
         
